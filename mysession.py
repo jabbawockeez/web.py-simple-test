f51ec7b7935ceb5679fae8c222a7fa90de9f61e0
jabba <760608972@qq.com>
2015-10-20 10:50:26 +0800

    add session

---
+++ mysession.py
@@ -0,0 +1,26 @@
+#import web
+import memcache
+
+#class MemCacheStore(web.session.Store):
+class MemCacheStore():
+    mc = None
+
+    def __init__(self):
+        self.mc = memcache.Client(['192.168.80.1:11211'], debug = 0)
+
+    def __contains__(self, key):
+        return self.mc.get(key) != None
+
+    def __getitem__(self, key):
+        return self.mc.get(key)
+
+    def __setitem__(self, key, value):
+        self.mc.set(key, value, 10)
+        #self.mc.set(key, value, time = web.config.session_parameters["timeout"])
+
+    def __delitem__(self, key):
+        self.mc.delete(key)
+
+    def cleanup(self, timeout):
+        #pass # Not needed as we assigned the timeout to memcache
+        self.mc.delete('username')
